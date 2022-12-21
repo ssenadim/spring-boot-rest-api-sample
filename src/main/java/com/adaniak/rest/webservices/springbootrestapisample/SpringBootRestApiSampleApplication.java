8e58cb23b5193bed4086da50ef07b664900e28e8
Süleyman Şenadım <suleyman@senadim.com>
2022-12-21 23:57:01 +0300

    Sample project

---
+++ src/main/java/com/adaniak/rest/webservices/springbootrestapisample/SpringBootRestApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.adaniak.rest.webservices.springbootrestapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adaniak/rest/webservices/springbootrestapisample/SpringBootRestApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaniak.rest.webservices.springbootrestapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
